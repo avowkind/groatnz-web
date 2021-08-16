7ac0a30c140b0af7d202b5b2576893f770644cc2
avowkind <andrew@groat.nz>
2021-08-17 09:13:53 +1200

    add nojekyll

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=PT+Sans:300,400,700,800"
-            rel="stylesheet"
-          />
-        </Head>
         <body>
           <Main />
           <NextScript />
