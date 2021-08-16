95375007cedf03d948677bbfa6f43678b1ad38c9
avowkind <andrew@groat.nz>
2021-08-17 10:21:11 +1200

    fix spelling

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import '../styles/normalize.css';
 import '../styles/skeleton.css';
 import '../styles/base.css';
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
